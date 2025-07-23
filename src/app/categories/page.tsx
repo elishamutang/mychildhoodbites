e36918d9903088c79a24e973f29812ef7f4c6976
elishamutang <elishamutang@outlook.com>
2025-07-23 21:00:06 +0800

    update: Added pages for products and categories.

---
+++ src/app/categories/page.tsx
@@ -0,0 +1,6 @@
+import { BlurFade } from "@/components/magicui/blur-fade";
+import Heading from "@/components/heading";
+
+export default function Page() {
+  return <Heading delay={0.25}>Categories.</Heading>;
+}
---
+++ src/app/products/page.tsx
@@ -0,0 +1,5 @@
+import Heading from "@/components/heading";
+
+export default function Page() {
+  return <Heading delay={0.25}>Products.</Heading>;
+}
