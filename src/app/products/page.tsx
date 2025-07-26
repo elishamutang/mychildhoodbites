004d90869b2501077959f47d4dc887c2d9b4eb29
elishamutang <elishamutang@outlook.com>
2025-07-26 17:30:20 +0800

    update: Pulled product data into Products page.

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,45 +1,23 @@
 import Heading from "@/components/heading";
-import { PrismaClient, Product } from "../../../generated/prisma";
+import { PrismaClient } from "../../../generated/prisma";
 import Card from "@/components/card";
 
 const prisma = new PrismaClient();
 
 export default async function Page() {
-  // const products = await prisma.product.findMany();
-
-  const test = [
-    {
-      name: "Ais Krim",
-      description: "Malaysian local ice creamas bruh bruh bruh.",
-      country: "Malaysia",
-      category: "Frozen Foods & Treats",
+  const products = await prisma.product.findMany({
+    include: {
+      Country: true,
+      Category: true,
     },
-    {
-      name: "Ais Krim",
-      description: "Malaysian local ice creamas bruh bruh bruh.",
-      country: "Malaysia",
-      category: "Frozen Foods & Treats",
-    },
-    {
-      name: "Ais Krim",
-      description: "Malaysian local ice creamas bruh bruh bruh.",
-      country: "Malaysia",
-      category: "Frozen Foods & Treats",
-    },
-    {
-      name: "Ais Krim",
-      description: "Malaysian local ice creamas bruh bruh bruh.",
-      country: "Malaysia",
-      category: "Frozen Foods & Treats",
-    },
-  ];
+  });
 
   return (
-    <section className="flex flex-col items-center border rounded-lg">
+    <section className="flex flex-col items-center md:border rounded-lg">
       <Heading delay={0.25}>Products.</Heading>
       <section className="p-4 w-full justify-start flex flex-col gap-3 md:grid grid-rows-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-3">
-        {test.map((i, idx) => {
-          return <Card test={i} key={idx} />;
+        {products.map((product) => {
+          return <Card product={product} key={product.id} />;
         })}
       </section>
     </section>
