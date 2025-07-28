af97712c5b79495dfefa14ff10c64334cd35de00
elishamutang <elishamutang@outlook.com>
2025-07-28 19:37:41 +0800

    update: Updated styling.

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -12,7 +12,9 @@ export default async function Page() {
 
   return (
     <section className="flex flex-col items-center md:border rounded-lg">
-      <Heading delay={0.25}>Products.</Heading>
+      <Heading delay={0.25} className="text-blue-600">
+        Bites<span className="text-green-600">.</span>
+      </Heading>
       <section className="p-4 w-full justify-start flex flex-col gap-3 md:grid grid-rows-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-3">
         {products.map((product) => {
           return <Card product={product} key={product.id} />;
