969237410304a7c6c005e9ffdf2739e9da9a6e7a
elishamutang <elishamutang@outlook.com>
2025-07-29 01:09:45 +0800

    update: Updated navbar.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,14 +4,25 @@ import { useDisclosure } from "@mantine/hooks";
 import { Burger } from "@mantine/core";
 import Link from "next/link";
 import { BlurFade } from "./magicui/blur-fade";
+import { MouseEventHandler, useEffect, useRef } from "react";
 
 export default function Nav() {
   const [opened, { toggle }] = useDisclosure();
+  const navElem = useRef<HTMLElement>(null);
+
+  const handleClick: MouseEventHandler<HTMLAnchorElement> = (event) => {
+    if (navElem) {
+      navElem.current?.classList.toggle("top-0");
+      navElem.current?.classList.toggle("-top-500");
+      toggle();
+    }
+  };
 
   return (
     <>
       <Burger
         className="mx-2 z-2 md:hidden"
+        color="gray"
         opened={opened}
         onClick={toggle}
         aria-label="Toggle navigation"
@@ -34,12 +45,24 @@ export default function Nav() {
 
       {/* Show overlay */}
       <nav
+        ref={navElem}
         className={`${
           opened ? "top-0 backdrop-blur-xs" : "-top-500"
         } left-0 z-1 ease-in-out transition-all gap-3 duration-300 flex flex-col items-center justify-center bg-white text-black absolute w-full h-screen`}
       >
-        <h1 className="text-2xl font-inter font-bold">Products</h1>
-        <h1 className="text-2xl font-inter font-bold">Categories</h1>
+        <Link
+          href="/products"
+          className="text-2xl font-inter font-bold text-blue-600"
+          onClick={handleClick}
+        >
+          Bites
+        </Link>
+        <Link
+          href="/categories"
+          className="text-2xl font-inter font-bold text-green-600"
+        >
+          Categories
+        </Link>
       </nav>
     </>
   );
