7c3b815a2306f84b01511868d743a5cc2d5ea87e
elishamutang <elishamutang@outlook.com>
2025-07-30 00:28:20 +0800

    update: Updated footer.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import { BlurFade } from "./magicui/blur-fade";
 
 export default function Footer() {
   return (
-    <footer className="border border-zinc-300 font-inter font-semibold rounded-lg w-full text-center">
+    <footer className="border text-xs md:text-sm border-zinc-300 font-inter font-semibold rounded-lg w-full text-center">
       <BlurFade inView className="border border-transparent">
         Designed and developed by{" "}
         <a
