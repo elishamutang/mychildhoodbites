e90edfa9117fdba54de6357651aaa207b6ae29f9
elishamutang <elishamutang@outlook.com>
2025-07-30 23:46:34 +0800

    update: Updated individual product page.

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from "next/navigation";
 import { prisma } from "@/lib/prisma";
 import Image from "next/image";
 import getProductImg from "@/lib/getProductImg";
+import Flag from "@/components/flag";
 
 export default async function Page({
   params,
@@ -26,39 +27,55 @@ export default async function Page({
   const imgSrc = await getProductImg(product.name);
 
   return (
-    <section className="font-inter flex flex-col gap-5 border md:border rounded-lg p-5">
-      {/* Title */}
-      <h1 className="font-extrabold text-5xl">
-        {product.name}
-        <span className="text-green-600">.</span>
-      </h1>
+    <section className="font-inter md:flex-row flex flex-col gap-2 border rounded-lg p-5">
+      {/* Left */}
+      <section className="flex-2 flex flex-col gap-2">
+        {/* Title */}
+        <h1 className="font-extrabold mb-2 text-5xl">
+          {product.name}
+          <span className="text-green-600">.</span>
+        </h1>
 
-      {/* Category */}
-      <section className="border w-max px-2 py-1 rounded-sm bg-blue-600 font-semibold text-white text-xs">
-        {product.Category.name}
-      </section>
-
-      {/* Image */}
-      {imgSrc && (
-        <Image
-          src={imgSrc}
-          width={500}
-          height={500}
-          alt={product.name}
-          priority={true}
+        <Flag
+          tooltipWidth="w-[40px]"
+          src={product.Country.flag}
+          width={40}
+          height={40}
+          alt={`${product.Country.name} flag`}
+          countryName={product.Country.name}
+          className="drop-shadow-xl/20 mb-2"
         />
-      )}
 
-      {/* Brief Description */}
-      <section className="flex flex-col gap-2">
-        <h2 className="text-3xl font-bold">Description</h2>
-        <p>{product.description}</p>
+        {/* Category */}
+        <section className="border w-max px-2 py-1 rounded-sm bg-blue-600 font-semibold text-white text-xs">
+          {product.Category.name}
+        </section>
+
+        {/* Image */}
+        {imgSrc && (
+          <Image
+            src={imgSrc}
+            width={500}
+            height={500}
+            alt={product.name}
+            priority={true}
+          />
+        )}
       </section>
 
-      {/* Lore */}
-      <section className="flex flex-col gap-2">
-        <h2 className="text-3xl font-bold">Lore</h2>
-        <p>{product.lore}</p>
+      {/* Right */}
+      <section className="flex-3 flex flex-col gap-3 px-8 md:border-l">
+        {/* Brief Description */}
+        <section className="flex flex-col gap-2">
+          <h2 className="text-3xl lg:text-4xl font-bold">Description</h2>
+          <p className="text-zinc-500">{product.description}</p>
+        </section>
+
+        {/* Lore */}
+        <section className="flex flex-col gap-2">
+          <h2 className="text-3xl lg:text-4xl font-bold">Lore</h2>
+          <p className="text-zinc-500">{product.lore}</p>
+        </section>
       </section>
     </section>
   );
