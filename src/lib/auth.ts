95595ce5b1b16cf93848751e8245f8ed6ade6b18
elishamutang <elishamutang@outlook.com>
2025-08-06 21:51:07 +0800

    update: Added cookies plugin for NextJS in better-auth.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 // If your Prisma file is located elsewhere, you can change the path
 import { PrismaClient } from "../../generated/prisma";
+import { nextCookies } from "better-auth/next-js";
 
 const prisma = new PrismaClient();
 export const auth = betterAuth({
@@ -16,5 +17,6 @@ export const auth = betterAuth({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }
-    }
+    },
+    plugins: [nextCookies()]
 });
