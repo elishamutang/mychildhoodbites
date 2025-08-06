6fe0ca5f2d54357f3dd5c71bd0288c1b4bbe8d69
elishamutang <elishamutang@outlook.com>
2025-08-06 21:49:32 +0800

    update: Added sign in server action for user sign-in.

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,9 +1,20 @@
 "use server"
 
 import { z } from 'zod'
+import { auth } from '@/lib/auth'
+import { headers } from 'next/headers'
+import { APIError } from 'better-auth/api'
+import { SignInData, SignInResult } from '@/lib/types'
 
 const Product = z.object({
     product: z.string()
+})
+
+const SignInSchema = z.object({
+    email: z.email({
+        error: 'Invalid email address.'
+    }).nonempty(),
+    password: z.string().nonempty({ error: 'Password must not be empty.' }).min(8, { error: 'Password must be at least 8 characters.' })
 })
 
 export async function getProduct(initialState: any, formData: FormData) {
@@ -19,4 +30,52 @@ export async function getProduct(initialState: any, formData: FormData) {
             }
         }, 2000)
     })
+}
+
+// Handle user sign in.
+export async function signIn(initialState: any, formData: FormData): Promise<SignInResult> {
+
+    const rawData: SignInData = {
+        email: formData.get('email') as string,
+        password: formData.get('password') as string
+    }
+
+    const validated = SignInSchema.safeParse(rawData)
+
+    if (!validated.success) {
+        return {
+            success: false,
+            message: 'Please fix the errors in the form.',
+            errors: z.flattenError(validated.error),
+            inputs: { ...rawData, password: '' }
+        }
+    }
+
+    try {
+        await auth.api.signInEmail({
+            body: {
+                email: validated.data.email,
+                password: validated.data.password,
+                callbackURL: '/dashboard'
+            },
+            headers: await headers()
+        })
+
+        console.log("User signed in: ", validated.data.email)
+        return { success: true }
+    } catch (error) {
+        if (error instanceof APIError) {
+            return {
+                success: false,
+                message: error.message,
+                statusCode: error.statusCode
+            }
+        }
+
+        return {
+            success: false,
+            message: 'Unexpected server error. Please try again.',
+            inputs: { ...rawData, password: '' }
+        }
+    }
 }
