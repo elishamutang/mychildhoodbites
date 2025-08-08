d6059b61d5d893e29c1eb7b84fbf0acdcf2a5d4a
elishamutang <elishamutang@outlook.com>
2025-08-09 01:13:32 +0800

    update: Updated sign out button.

--- src/components/signOut.tsx
+++ src/components/signOut.tsx
@@ -1,15 +1,26 @@
 "use client";
 
 import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
 
 export default function SignOut() {
+  const router = useRouter();
+
   return (
     <button
-      onClick={async () => await authClient.signOut()}
+      onClick={async () =>
+        await authClient.signOut({
+          fetchOptions: {
+            onSuccess: () => {
+              router.push("/");
+            },
+          },
+        })
+      }
       type="button"
       className="border px-2 py-1 rounded-sm bg-blue-600 text-white cursor-pointer"
     >
-      Sign Out
+      <span className="font-bold">Sign Out</span>
     </button>
   );
 }
