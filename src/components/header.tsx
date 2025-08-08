8861220dab9a1817fef254d1b6906f0fa5e9129b
elishamutang <elishamutang@outlook.com>
2025-08-09 00:17:59 +0800

    update: Removed session check on the server parent component.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { BlurFade } from "./magicui/blur-fade";
 import Nav from "./navbar";
 
-export default function Header() {
+export default async function Header() {
   return (
     <header className="flex border-zinc-300 items-center justify-between border rounded-lg w-full p-2">
       <Link href="/" className="text-2xl font-extrabold font-inter">
