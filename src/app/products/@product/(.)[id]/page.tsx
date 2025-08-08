543c500822639e42cae697e69968a34031508e85
elishamutang <elishamutang@outlook.com>
2025-08-08 22:18:57 +0800

    update: Removed form element.

--- src/app/products/@product/(.)[id]/page.tsx
+++ src/app/products/@product/(.)[id]/page.tsx
@@ -31,43 +31,41 @@ export default async function Page({
 
   return (
     <Dialog defaultOpen={true}>
-      <form className="hidden absolute">
-        <DialogContent className="sm:max-w-[425px] font-inter">
-          <DialogHeader>
-            <DialogTitle className="text-3xl md:text-4xl font-bold text-start md:text-center">
-              {product.name}
-            </DialogTitle>
-            {imgSrc && (
-              <Image
-                objectFit="contain"
-                className="self-center"
-                src={imgSrc ?? null}
-                width={400}
-                height={400}
-                alt={product.name}
-              />
-            )}
-            <h2 className="text-lg font-semibold text-start">Description</h2>
-            <DialogDescription className="text-start">
-              {product.description}
-            </DialogDescription>
+      <DialogContent className="sm:max-w-[425px] font-inter">
+        <DialogHeader>
+          <DialogTitle className="text-3xl md:text-4xl font-bold text-start md:text-center">
+            {product.name}
+          </DialogTitle>
+          {imgSrc && (
+            <Image
+              objectFit="contain"
+              className="self-center"
+              src={imgSrc ?? null}
+              width={400}
+              height={400}
+              alt={product.name}
+            />
+          )}
+          <h2 className="text-lg font-semibold text-start">Description</h2>
+          <DialogDescription className="text-start">
+            {product.description}
+          </DialogDescription>
 
-            <h2 className="text-lg font-semibold text-start">Lore</h2>
-            <DialogDescription className="text-start">
-              {product.lore}
-            </DialogDescription>
-          </DialogHeader>
-          <DialogFooter>
-            <Link
-              className="text-sm border text-center py-1 border-zinc-300 px-2 rounded-sm hover:font-semibold hover:text-white hover:bg-green-700 transition duration-200"
-              href={`/products/${id}`}
-              target="_blank"
-            >
-              Read More
-            </Link>
-          </DialogFooter>
-        </DialogContent>
-      </form>
+          <h2 className="text-lg font-semibold text-start">Lore</h2>
+          <DialogDescription className="text-start">
+            {product.lore}
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Link
+            className="text-sm border text-center py-1 border-zinc-300 px-2 rounded-sm hover:font-semibold hover:text-white hover:bg-green-700 transition duration-200"
+            href={`/products/${id}`}
+            target="_blank"
+          >
+            Read More
+          </Link>
+        </DialogFooter>
+      </DialogContent>
     </Dialog>
   );
 }
