bb71c9d28d7b23321be2b02342dc0e7b1c194212
elishamutang <elishamutang@outlook.com>
2025-08-09 15:52:56 +0800

    update: Updated product modal for mobile.

--- src/app/products/@product/(.)[id]/page.tsx
+++ src/app/products/@product/(.)[id]/page.tsx
@@ -31,28 +31,28 @@ export default async function Page({
 
   return (
     <Dialog defaultOpen={true}>
-      <DialogContent className="sm:max-w-[425px] font-inter">
+      <DialogContent className="sm:max-w-[425px] h-content font-inter">
         <DialogHeader>
-          <DialogTitle className="text-3xl md:text-4xl font-bold text-start md:text-center">
+          <DialogTitle className="text-3xl md:text-4xl font-bold md:text-center">
             {product.name}
           </DialogTitle>
           {imgSrc && (
             <Image
               objectFit="contain"
-              className="self-center"
+              className="self-center w-60 h-60 md:w-70 h-70"
               src={imgSrc ?? null}
               width={400}
               height={400}
               alt={product.name}
             />
           )}
-          <h2 className="text-lg font-semibold text-start">Description</h2>
-          <DialogDescription className="text-start">
+          <h2 className="mt-1 text-lg font-semibold text-start">Description</h2>
+          <DialogDescription className="text-start h-10 line-clamp-2 md:line-clamp-none md:h-max">
             {product.description}
           </DialogDescription>
 
           <h2 className="text-lg font-semibold text-start">Lore</h2>
-          <DialogDescription className="text-start">
+          <DialogDescription className="text-start h-10 line-clamp-2 md:line-clamp-none md:h-max">
             {product.lore}
           </DialogDescription>
         </DialogHeader>
